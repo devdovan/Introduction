d55faccd7cedc32c10271f40be127bdd35f87744
Busra Dovan <devdovanbusra@outlook.com>
2025-05-10 11:42:13 +0300

    Add project files.

---
+++ 01-FirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01_FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa Hoşgeldiniz...");
+            Console.Write("Adınız: ");
+
+            string name = Console.ReadLine();
+            Console.Write("Hoşgeldin ");
+            Console.WriteLine(name);
+
+        }
+    }
+}
