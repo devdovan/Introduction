7bcbcd3bb031f656528df5b0c78295f8146dd627
Busra Dovan <devdovanbusra@outlook.com>
2025-05-27 18:00:14 +0300

    AsalSayiKontrolu

---
+++ 16-LABAsalSayi/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _16_LABAsalSayi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Bir sayı giriniz: ");
+                int sayi = int.Parse(Console.ReadLine());
+                Console.Write("Asal mı?  ");
+                bool asalMi = true;
+                if (sayi < 2)
+                {
+                    asalMi = false;
+                }
+
+                for (int i = 2; i <= Math.Sqrt(sayi); i++)
+                {
+                    if (sayi % i == 0)
+                    {
+                        asalMi = false;
+
+                    }
+                }
+
+                Console.WriteLine(asalMi);
+
+            }
+        }
+    }
+}
